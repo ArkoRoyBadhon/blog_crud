0590a6b7b462937e62ebe7b10da90e7669309d1c
Arko <arkoroyb@gmail.com>
2024-06-24 11:51:35 +0600

    some changes

--- src/routes/v1/category.route.ts
+++ src/routes/v1/category.route.ts
@@ -6,13 +6,13 @@ import {
   getCategoryByIdController,
   updateCategoryController,
 } from "../../controllers/category.controller";
-import { authorizeRoles } from "../../middlewares/auth";
+import { authorizeRoles, isAuthenticatedUser } from "../../middlewares/auth";
 import { validateArticle } from "../../helpers/validArticle";
 const router = express.Router();
 
-router.post("/c/create", validateArticle, authorizeRoles("admin"), createCategoryController);
-router.delete("/c/delete/:id", validateArticle, authorizeRoles("admin"), deleteCategoryController);
-router.patch("/c/update/:id", validateArticle, authorizeRoles("admin"), updateCategoryController);
+router.post("/c/create", isAuthenticatedUser, authorizeRoles("admin"), createCategoryController);
+router.delete("/c/delete/:id", isAuthenticatedUser, authorizeRoles("admin"), deleteCategoryController);
+router.patch("/c/update/:id", authorizeRoles("admin"), updateCategoryController);
 router.get("/c/get", getCategoriesController);
 router.get("/c/get/:id", getCategoryByIdController);
 
