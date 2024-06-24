f0591e0f12158eb246a8ca66a3bcc947813495f0
RiaJul Pro <riajulpro@outlook.com>
2024-06-24 09:58:30 +0600

    building again

--- dist/routes/v1/application.route.js
+++ dist/routes/v1/application.route.js
@@ -4,11 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const comment_controller_1 = require("../../controllers/comment.controller");
-const validArticle_1 = require("../../helpers/validArticle");
 const auth_1 = require("../../middlewares/auth");
+const application_controller_1 = require("../../controllers/application.controller");
 const router = express_1.default.Router();
 // employee personal Data
-router.post("/c/create", validArticle_1.validateArticle, comment_controller_1.createCommentController);
-router.delete("/c/delete/:id", validArticle_1.validateArticle, (0, auth_1.authorizeRoles)("admin"), comment_controller_1.deleteCommentController);
+router.post("/a/create", auth_1.isAuthenticatedUser, (0, auth_1.authorizeRoles)("regular"), application_controller_1.createApplicationController);
+router.post("/a/update", auth_1.isAuthenticatedUser, (0, auth_1.authorizeRoles)("admin"), application_controller_1.updateUserRoleController);
 exports.default = router;
--- dist/routes/v1/people.route.js
+++ dist/routes/v1/people.route.js
@@ -4,10 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const people_controller_1 = require("../../controllers/people.controller");
-const validPeople_1 = require("../../helpers/validPeople");
+const auth_controller_1 = require("../../controllers/auth.controller");
 const router = express_1.default.Router();
 // employee personal Data
-router.post("/a/create", validPeople_1.validatePeople, people_controller_1.CreatePeopleController);
-router.get("/all/get", people_controller_1.getAllPeople);
+// router.post("/a/create",validatePeople, CreatePeopleController);
+router.post("/register", auth_controller_1.registerUserController);
+router.post("/login", auth_controller_1.signinController);
+// router.get("/all/get", getAllPeople);
 exports.default = router;
