227ce777621662a83f61208e1c8de0b1ad800102
sakibsarkar <sakibsarkar707@gmail.com>
2024-06-24 12:35:29 +0600

    commnet validation fix

--- dist/routes/v1/comment.route.js
+++ dist/routes/v1/comment.route.js
@@ -5,10 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const comment_controller_1 = require("../../controllers/comment.controller");
-const validArticle_1 = require("../../helpers/validArticle");
 const auth_1 = require("../../middlewares/auth");
 const router = express_1.default.Router();
 // employee personal Data
-router.post("/c/create", validArticle_1.validateArticle, comment_controller_1.createCommentController);
-router.delete("/c/delete/:id", validArticle_1.validateArticle, (0, auth_1.authorizeRoles)("admin"), comment_controller_1.deleteCommentController);
+router.post("/c/create", comment_controller_1.createCommentController);
+router.delete("/c/delete/:id", (0, auth_1.authorizeRoles)("admin"), comment_controller_1.deleteCommentController);
 exports.default = router;
